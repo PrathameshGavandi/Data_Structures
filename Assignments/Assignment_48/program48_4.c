e0a660bec3659ca5f76c68d561b6d5ec76e470f3
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-12-30 23:41:01 +0530

    Add files via upload

---
+++ Assignments/Assignment_48/program48_4.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void DisplayEvenPosition(PNODE first)
+{
+    int iPos = 1;
+    while (first != NULL)
+    {
+        if (iPos % 2 == 0)
+        {
+            printf("%d\t", first->data);
+        }
+
+        first = first->next;
+        iPos++;
+    }
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | ->", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    //
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 20);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 50);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 24);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes after InsertFirst are : %d\n", iRet);
+    printf("-------------------------------------------------------------\n");
+
+    //
+    DisplayEvenPosition(head);
+
+    return 0;
+}
