492eda6774d051927df1632a5a1f3a6c5b13c1a8
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-12-30 11:36:22 +0530

    Add files via upload

---
+++ Assignments/Assignment_46/program46_3.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+bool IsEmpty(PNODE first)
+{
+    bool IsEmpty = false;
+
+    if (first == NULL)
+    {
+        IsEmpty = true;
+    }
+
+    return IsEmpty;
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | ->", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+    bool bRet = false;
+
+    //
+    // InsertFirst(&head, 51);
+    // InsertFirst(&head, 20);
+    // InsertFirst(&head, 11);
+    // InsertFirst(&head, 50);
+    // InsertFirst(&head, 22);
+    // InsertFirst(&head, 11);
+    // InsertFirst(&head, 24);
+
+    //Display(head);
+    iRet = Count(head);
+    printf("Number of nodes after InsertFirst are : %d\n", iRet);
+    printf("-------------------------------------------------------------\n");
+
+    //
+    bRet = IsEmpty(head);
+
+    if(bRet == true)
+    {
+        printf("List is Empty\n");
+    }
+    else
+    {
+        printf("List has One or more Elements\n");
+    }
+
+
+    return 0;
+}
