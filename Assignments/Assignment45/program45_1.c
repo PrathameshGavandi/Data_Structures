ffd12e1ad50f00e3580b7951398ef26dd818476f
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-12-30 11:10:48 +0530

    Implement linked list with insertion and display Odd elements from the list
    This program implements a singly linked list with functions to insert nodes at the beginning, display all nodes, display only odd data values, and count the total number of nodes.

---
+++ Assignments/Assignment45/program45_1.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE) malloc (sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+}
+
+void DisplayOdd(PNODE first)
+{
+    while (first != NULL)
+    {
+        if((first -> data) % 2 != 0)
+        {
+            printf("%d\t",first -> data);
+        }
+        first = first -> next;
+    }
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | ->", first -> data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;  
+}
+
+
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    //
+    InsertFirst(&head,51);
+    InsertFirst(&head,20);
+    InsertFirst(&head,11);
+    InsertFirst(&head,50);
+    InsertFirst(&head,22);
+    InsertFirst(&head,11);
+    InsertFirst(&head,24);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes after InsertFirst are : %d\n",iRet);
+    printf("-------------------------------------------------------------\n");
+
+    //
+    DisplayOdd(head);
+    
+
+    return 0;
+}
