b8f758920d656ddc1aeddd015caeddde845852ca
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-12-30 11:13:37 +0530

    Implemented linked list with operations like insertion,Display.
    Implemented linked list with operations like insertion ,Display Odd elements , Display Even Elements from the list

---
+++ Assignments/Assignment45/program45_4.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+int CountGreater(PNODE first, int X)
+{
+    int iCount = 0;
+    while (first != NULL)
+    {
+        if (first->data > X)
+        {
+            iCount++;
+        }
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | ->", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    //
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 20);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 50);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 24);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes after InsertFirst are : %d\n", iRet);
+    printf("-------------------------------------------------------------\n");
+
+    //
+    iRet = CountGreater(head,15);
+
+    if (iRet < 1)
+    {
+        printf("Number not found\n");
+    }
+    else
+    {
+        printf("Count of numbers Greater than X is : %d\n", iRet);
+    }
+
+    return 0;
+}
