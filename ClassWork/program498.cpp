c59ea1aa79c78ac5aa543b7f642aa48764a7dba7
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-02-04 22:09:36 +0530

    Add files via upload

---
+++ ClassWork/program498.cpp
@@ -0,0 +1,171 @@
+//////////////////////////////////////////////////////////////////
+//Final code of Queue using Generic Approach
+//////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+#pragma pack(1)
+template<class T>
+class Queuenode
+{
+public:
+    T data;
+    Queuenode<T> *next;
+
+    Queuenode(T no)
+    {
+        this->data = no;
+        this->next = NULL;
+    }
+};
+
+template<class T>
+class Queue
+{
+private:
+    Queuenode<T> *first;
+    Queuenode<T> *last;
+
+    int iCount;
+
+public:
+    Queue();
+
+    void enqueue(T);
+    T dequeue(); 
+    void Display();
+    int Count();
+};
+
+template<class T>
+Queue<T> :: Queue()
+{
+    cout << "Queue Gets Created Successfully...\n";
+
+    this->first = NULL;
+    this->last = NULL;
+    this->iCount = 0;
+}
+
+template<class T>
+void Queue<T> :: enqueue(T no)
+{
+    Queuenode<T> *newn = NULL;
+
+    newn = new Queuenode<T>(no);
+
+    if(this->first == NULL && this->last == NULL)
+    {
+        this->first = newn;
+        this->last = newn;
+    }
+    else
+    {
+        this->last->next = newn;
+        this->last = newn;
+        
+    }
+    this->iCount++;
+    
+}
+
+template<class T>
+T Queue<T> :: dequeue()
+{
+    T Value = 0;
+    Queuenode<T> *temp = this->first;
+
+    if (this->first == NULL && this->last == NULL)
+    {
+        cout << "Queue is Empty\n";
+        return -1;
+    }
+
+    Value = this->first->data;
+
+    this->first = this->first->next;
+    delete temp;
+
+    this->iCount--;
+
+    return Value;
+}
+
+template<class T>
+void Queue<T> :: Display()
+{
+    Queuenode<T> *temp = this->first;
+
+    if (this->first == NULL && this->last == NULL)
+    {
+        cout << "Queue is empty\n";
+        return;
+    }
+
+    while (temp != NULL)
+    {
+        cout << "| " << temp->data << " | - ";
+        temp = temp->next;
+    }
+
+    cout<<"\n";
+}
+
+template<class T>
+int Queue<T> ::Count()
+{
+    return this->iCount;
+}
+
+int main()
+{
+
+    Queue<int> *sobj = new Queue<int>();
+
+    // PUSH
+    sobj->enqueue(11);
+    sobj->enqueue(21);
+    sobj->enqueue(51);
+    sobj->enqueue(101);
+
+    sobj->Display();
+
+    cout << "Number of elements in Queue are : " << sobj->Count() << "\n";
+
+    cout<<"------------------------------------------------------------------\n";
+
+
+    cout<<"Removed element is : "<<sobj->dequeue()<<"\n";
+
+    sobj->Display();
+
+    cout << "Number of elements in Stack are : " << sobj->Count() << "\n";
+
+    cout<<"------------------------------------------------------------------\n";
+
+    cout<<"Removed element is : "<<sobj->dequeue()<<"\n";
+
+    sobj->Display();
+
+    cout << "Number of elements in Stack are : " << sobj->Count() << "\n";
+
+    cout<<"------------------------------------------------------------------\n";
+
+    cout<<"Removed element is : "<<sobj->dequeue()<<"\n";
+
+    sobj ->enqueue(121);
+
+    sobj->Display();
+
+    cout << "Number of elements in Stack are : " << sobj->Count() << "\n";
+
+
+    
+    cout << "--------------------------------------------------------------\n";
+
+
+    delete sobj;
+
+    return 0;
+}
